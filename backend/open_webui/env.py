4defd4b2255cbda6d6420621c5e32bb23e73479e
gray <mail@grayxu.cn>
2025-09-10 03:25:48 +0800

    chore: simplify

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -88,10 +88,10 @@ if "cuda_error" in locals():
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+# if WEBUI_NAME != "Open WebUI":
+#     WEBUI_NAME += " (Open WebUI)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://www.grayxu.cn/favicon.ico"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
